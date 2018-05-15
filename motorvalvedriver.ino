cc2ff57e7d286de17cfc6a9162582d328d0ec4d5
Bernt Hustad Hembre <bhhe@hvl.no>
2018-05-15 10:25:44 +0200

    Added serial debug info. Ready for testing.

--- motorvalvedriver.ino
+++ motorvalvedriver.ino
@@ -1,5 +1,12 @@
 /**
  * @brief Driver for exhaustvalve for motor. HVL project spring 2018
+ * 
+ * Set up for test: 
+ * Expect 1440 counts pr revolution.
+ * onValue  = 360
+ * offValue = 720
+ * Prints the counted number of pulses pr revolution to the serial port
+ * 
 **/
 
 
@@ -7,8 +14,35 @@
 #define encoderPinB 3
 #define outputPin 13 // LED pin for test
 
+volatile unsigned int onValue = 30000; // Very high values, effectively disabled by default
+volatile unsigned int offValue = 30001;
 volatile unsigned int pulsecounter = 0;
+volatile unsigned int lastcount = 0;
 
+
+/**
+ * @brief Analouge Comparator service routine
+ * Resets the interrupt counter for inputs A and B.
+**/
+ISR (ANALOG_COMP_vect) {
+  cli();
+  lastcount = pulsecounter;
+  pulsecounter = 0;
+  sei();
+}
+
+/**
+ * @brief Interrupt routine for the A and B interrupt lines. 
+ * Both interrupt sources are handled by the same inerrupt service routine. 
+**/
+void doEncoder() {
+  pulsecounter++;
+   if(pulsecounter == onValue){
+    digitalWrite(outputPin, HIGH);
+  } else if(pulsecounter == offValue){
+    digitalWrite(outputPin, LOW);
+  }
+}
 
 /**
  * @brief Set up
@@ -20,38 +54,36 @@ void setup() {
   pinMode(encoderPinA, INPUT);
   pinMode(encoderPinB, INPUT);
 
- /// Setup doEncoder to service both A and B signal interrupt on change.
+ /// Set up doEncoder to service both A and B signal interrupt on change.
   attachInterrupt(0, doEncoder, CHANGE);
   attachInterrupt(1, doEncoder, CHANGE);
 
-  Serial.begin (115200);
-}
+/// Set up analouge comparator interrupt
+  ADCSRB = ADCSRB & ! bit(ACME);   // disable Analog Comparator Multiplexer Enable
+  ACSR =  bit(ACI)                 // clear Analog Comparator Interrupt Flag
+        | bit(ACIE)                // set Analog Comparator Interrupt Enable
+        | bit(ACIS1) | bit(ACIS0)  // select rising edge: ACIS1/ACIS0 Analog Comparator Interrupt Mode Select
+        ;
 
+  Serial.begin(115200);
+}
 
 /**
  * @todo Add Debugoutput on serial port
- * @todo Add serial port handler
  *
 **/
 void loop() {
   
-
-
-
-}
-
-
-
-
-/**
- * @brief Interrupt routine for the A and B interrupt lines. 
- * Both interrupt sources are handled by the same inerrupt service routine. 
-**/
-void doEncoder() {
-  pulsecounter++;
-  if(pulsecounter == onvalue){
-    digitalWrite(outputPin, HIGH);
-  } else if((pulsecounter == offvalue){
-    digitalWrite(outputPin, LOW);
+  if(lastcount){
+    Serial.print("Cnt: ");
+    Serial.println(lastcount, DEC);
+    lastcount = 0;
   }
+
+
 }
+
+
+
+
+
